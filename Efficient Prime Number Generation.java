4fb8817770c0d86f12a3c7a3549c3e5098a3f9fd
AKthePiedPiper <114767831+Arghya442@users.noreply.github.com>
2024-08-30 13:50:22 +0530

    Create Efficient Prime Number Generation.java
    Sieve of Eratosthenes: Create a boolean array representing numbers from 2 to the upper limit. Mark multiples of primes as non-prime. The remaining unmarked numbers are primes. This is a more efficient algorithm for generating prime numbers within a range.

---
+++ Efficient Prime Number Generation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberGenerator {
+    public static void generatePrimes(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            isPrime[i] = true;
+        }
+        isPrime[0] = isPrime[1] = false;
+
+        for (int p = 2; p * p <= n; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i <= n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the upper limit: ");
+        int n = scanner.nextInt();
+
+        generatePrimes(n);
+    }
+}
